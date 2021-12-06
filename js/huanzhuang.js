87296c42dc5f375ade01b64914d38a1a7796167b
ohliya <93039010+ohliya@users.noreply.github.com>
2021-12-06 17:44:15 +0800

    Add files via upload

---
+++ js/huanzhuang.js
@@ -0,0 +1,12 @@
+//function nv(){
+//      var arr = document.getElementById ("div1");
+//      // for(var i = 0;i<arr.length;i++){
+//        arr.style.backgroundColor = "orange";
+//      // }
+//    }
+function shang(){
+	var arr = document.getElementsByClassName ("nvy1");
+	for(var i = 0;i<arr.length;i++){
+          arr.style.visibility = "visible";
+           }
+}
